9dafcaf1fd59ef25fc503283792ca94b9eb829da
arthur <arthur-9527>
2025-05-02 06:53:48 +0000

    simplify frontend to basic Hello World

---
+++ front/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      backgroundColor: '#1a1b26',
+      color: 'white',
+      fontSize: '24px',
+      fontFamily: 'Arial, sans-serif'
+    }}>
+      Hello World
+    </div>
+  );
+}
+
+export default App; 
